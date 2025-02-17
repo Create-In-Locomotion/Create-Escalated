759d345b8eeb39a0e800c1282ece6e51ea20dccd
rbasamoyai <b33gy0sh@gmail.com>
2025-02-17 13:45:37 -0500

    improvement: Adjusted hitbox of narrow walkway

--- common/src/main/java/rbasamoyai/escalated/index/EscalatedShapes.java
+++ common/src/main/java/rbasamoyai/escalated/index/EscalatedShapes.java
@@ -1,6 +1,15 @@
 package rbasamoyai.escalated.index;
 
+import com.simibubi.create.foundation.utility.VoxelShaper;
+import net.minecraft.core.Direction;
+import net.minecraft.world.phys.shapes.Shapes;
+
+import static net.minecraft.world.level.block.Block.box;
+
 public class EscalatedShapes {
+
+    public static final VoxelShaper NARROW_WALKWAY = VoxelShaper.forHorizontal(Shapes.or(box(0, 0, 0, 16, 15.5, 16),
+            box(0, 15.5, 0, 2, 16, 16), box(14, 15.5, 0, 16, 16, 16)), Direction.NORTH);
 
     private EscalatedShapes() {}
 
--- common/src/main/java/rbasamoyai/escalated/walkways/NarrowWalkwayBlock.java
+++ common/src/main/java/rbasamoyai/escalated/walkways/NarrowWalkwayBlock.java
@@ -10,12 +10,16 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import rbasamoyai.escalated.index.EscalatedShapes;
 
 public class NarrowWalkwayBlock extends AbstractWalkwayBlock  {
 
@@ -114,6 +118,11 @@ public class NarrowWalkwayBlock extends AbstractWalkwayBlock  {
         }
 
         return super.onSneakWrenched(state, context);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext context) {
+        return EscalatedShapes.NARROW_WALKWAY.get(state.getValue(HORIZONTAL_FACING));
     }
 
 }
