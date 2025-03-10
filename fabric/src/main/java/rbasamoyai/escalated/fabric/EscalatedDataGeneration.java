52957a76720372879cff6166c26b55746ca31e0d
rbasamoyai <b33gy0sh@gmail.com>
2025-03-09 23:19:57 -0400

    feature: Added recipe for walkway belts
    - Added recipe generation boilerplate
    - TODO change forge recipe to c:iron_plates for 1.21

--- fabric/src/main/java/rbasamoyai/escalated/fabric/EscalatedDataGeneration.java
+++ fabric/src/main/java/rbasamoyai/escalated/fabric/EscalatedDataGeneration.java
@@ -7,12 +7,15 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator.Pack.Factory;
 import rbasamoyai.escalated.CreateEscalated;
 import rbasamoyai.escalated.datagen.EscalatedLangGen;
 import rbasamoyai.escalated.datagen.assets.fabric.EscalatedPartialsGen;
+import rbasamoyai.escalated.datagen.data.fabric.EscalatedCraftingRecipeProvider;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
 
 public class EscalatedDataGeneration implements DataGeneratorEntrypoint {
+
+	public static final String PLATFORM = System.getProperty("escalated.datagen.platform");
 
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator generator) {
@@ -27,6 +30,10 @@ public class EscalatedDataGeneration implements DataGeneratorEntrypoint {
 		modDatapack.addProvider((Factory<EscalatedPartialsGen>) output -> new EscalatedPartialsGen(output, CreateEscalated.MOD_ID, helper));
 
 		EscalatedLangGen.prepare();
+		EscalatedCraftingRecipeProvider.register();
 	}
+
+	public static boolean isForge() { return "forge".equals(PLATFORM); }
+	public static boolean isFabric() { return "fabric".equals(PLATFORM); }
 
 }
